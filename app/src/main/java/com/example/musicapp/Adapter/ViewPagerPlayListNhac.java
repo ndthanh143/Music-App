5c463a890a2aa83e26ad9d97331f8930248e1833
Vttien01 <tientienpham01@gmail.com>
2023-05-03 17:35:12 +0700

    create layout playmusic 2

--- app/src/main/java/com/example/musicapp/Adapter/ViewPagerPlayListNhac.java
+++ app/src/main/java/com/example/musicapp/Adapter/ViewPagerPlayListNhac.java
@@ -1,29 +1,55 @@
 package com.example.musicapp.Adapter;
 
+import android.widget.Switch;
+
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+
+import com.example.musicapp.Fragment.FragmentDiaNhac;
+import com.example.musicapp.Fragment.FragmentInfoNhac;
+import com.example.musicapp.Fragment.FragmentLyricsNhac;
 
 import java.util.ArrayList;
 
-public class ViewPagerPlayListNhac extends FragmentPagerAdapter {
-    public final ArrayList<Fragment> fragmentArrayList =new ArrayList<>();
-    public ViewPagerPlayListNhac(@NonNull FragmentManager fm) {
-        super(fm);
+public class ViewPagerPlayListNhac extends FragmentStatePagerAdapter {
+
+    public ViewPagerPlayListNhac(@NonNull FragmentManager fm, int behavior) {
+        super(fm, behavior);
     }
 
     @NonNull
     @Override
     public Fragment getItem(int position) {
-        return fragmentArrayList.get(position);
+        switch (position){
+            case 0:
+                return new FragmentInfoNhac();
+            case 1:
+                return new FragmentDiaNhac();
+            case 2:
+                return new FragmentLyricsNhac();
+            default:  return new FragmentInfoNhac();
+        }
     }
 
     @Override
     public int getCount() {
-        return fragmentArrayList.size();
+        return 3;
     }
-    public void AddFragment(Fragment fragment){
-        fragmentArrayList.add(fragment);
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        String title ="";
+        switch (position){
+            case 0: title = "Thông tin bài hát";
+            case 1: title = "Bài hát";
+            case 2: title = "Lời bài hát";
+            break;
+        }
+        return title;
     }
 }
